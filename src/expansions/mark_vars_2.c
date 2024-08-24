891d2e18c88f1ae072e543194c40ff8320c5fc65
SG-0205 <samgold0205@gmail.com>
2024-08-24 14:29:02 +0200

    save

--- src/expansions/mark_vars_2.c
+++ src/expansions/mark_vars_2.c
@@ -6,7 +6,7 @@
 /*   By: sgoldenb <sgoldenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 12:42:33 by sgoldenb          #+#    #+#             */
-/*   Updated: 2024/08/22 20:38:54 by sgoldenb         ###   ########.fr       */
+/*   Updated: 2024/08/24 14:28:25 by sgoldenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,30 +40,37 @@ char	**fill_var_names(char *to_expand, int var_count,
 void	place_var_sep(t_expand *exp, t_mshell *data)
 {
 	int		i;
+	int		j;
 	char	*vars_marked;
 
 	if (!exp || !exp->to_expand || exp->var_count < 1)
 		return ;
-	i = -1;
+	i = 0;
+	j = 0;
 	vars_marked = (char *)gc_strnew((sizeof(char)
 				* (ft_strlen(exp->to_expand) + exp->var_count)), data->gc, 0);
 	if (!vars_marked)
 		return ;
-	while (exp->to_expand[++i])
+	while (exp->to_expand[i]
+		&& j < (int)ft_strlen(exp->to_expand) + exp->var_count)
 	{
 		if (exp->to_expand[i] == '$'
 			&& validate_var(&exp->to_expand[i]) == TRUE)
-			vars_marked[i] = VAR_SEP;
-		else if (ft_cisar(exp->to_expand[i], EXPORT_FORBIDDEN_CHARS) != 0
-			|| ft_cisar(exp->to_expand[i], ECHO_ESCAPE_SEQUENCES)
-			|| exp->to_expand[i] == '$' || exp->to_expand[i] == ' ')
-			vars_marked[i] = VAR_SEP;
+		{
+			vars_marked[j] = VAR_SEP;
+			ft_strcat(vars_marked, get_var_name(&exp->to_expand[i], data));
+			vars_marked[ft_strlen(vars_marked)] = VAR_SEP;
+			j += (int)ft_strlen(&vars_marked[j]) + 1;
+		}
 		else
-			vars_marked[i] = exp->to_expand[i];
+			vars_marked[j] = exp->to_expand[i], i++, j++;
 	}
 	exp->to_expand = gc_strdup(vars_marked, data->gc, 0);
 	if (!exp->to_expand)
 		return ;
 }
 
+//FAIRE UN PUTAIN DE SCHEMA POUR CETTE PUTAIN DE CONCATENATION
+
 //TODO VERIFIER CONDITIONS DE DELIMITATION DES VARIABLES , SAVOIR QUE FAIRE DE &
+//& SERA CONSIDERE COMME EOV - 24/08
